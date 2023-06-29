a03bf644af689caa5b97dedd1de62d29d2c560c6
Techprenuer1 <125614142+Techprenuer1@users.noreply.github.com>
2023-06-29 12:41:19 +0000

    changed profile card Paige B

--- src/js/component/myprofile.js
+++ src/js/component/myprofile.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import "../../styles/myprofile.css";
 
+
 export const MyProfile = () => {
   return (
     <div className="container">
-      <body>
-        <div>
+     
+        {/* <div> */}
           {/* <p class="center">
             <em>Hi, Im Laura M.</em>
           </p>
@@ -18,7 +19,37 @@ export const MyProfile = () => {
             alt="..."
           />
           <p class="alltime-fave">My All Time Favorite Book is....</p> */}
-          
+          <div class="container mt-4 mb-4 p-3 d-flex justify-content-center"> 
+          <div class="card p-4"> 
+          <div class=" image d-flex flex-column justify-content-center align-items-center"> 
+          <button class="btn btn-secondary"> 
+          <img src="https://i.imgur.com/wvxPV9S.png" height="100" width="100" /></button> 
+          <span class="name mt-3">Eleanor Pena</span> 
+          <span class="idd">@eleanorpena</span> 
+          <div class="d-flex flex-row justify-content-center align-items-center gap-2"> 
+           <span><i class="fa-solid fa-location-dot"></i></span> 
+           </div> 
+           <div class="d-flex flex-row justify-content-center align-items-center mt-3"> 
+           <span class="number">1069 <span class="follow">Followers</span></span> </div> 
+           <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> 
+           </div> 
+           <div class="text mt-3"> 
+           <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br></br> Artist/ Creative Director by Day #NFT minting@ with FND night.</span> 
+           </div> 
+           <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> 
+           <span><i class="fa fa-twitter"></i></span>
+           <span><i class="fa-brands fa-facebook fa-bounce"></i></span> 
+           <span><i class="fa fa-instagram"></i></span> 
+           <span><i class="fa fa-linkedin"></i></span> 
+           </div> 
+           <div class=" px-2 rounded mt-4 date "> 
+           <span class="join">Joined May,2021</span> 
+           </div> 
+           </div>
+            </div>
+            
+</div>
+
           <div
             className="card mb-3"
             style={{ maxWidth: "540px" }}
@@ -140,7 +171,6 @@ export const MyProfile = () => {
             </div>
           </div>
         </div>
-      </body>
-    </div>
+ 
   );
 };
