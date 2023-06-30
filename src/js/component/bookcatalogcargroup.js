d6bd3ec3f90d58624e4130f99ce16845016df115
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-06-30 08:55:29 +0000

    working scroll bar on search bar, fixed expand button,catalog  page done?

--- src/js/component/bookcatalogcargroup.js
+++ src/js/component/bookcatalogcargroup.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 const CardGroup = ({ book }) => {
-  const [show, setShow] = useState(false);
   const [expandedIndex, setExpandedIndex] = useState(null);
 
   const handleExpandToggle = (index) => {
@@ -13,30 +12,41 @@ const CardGroup = ({ book }) => {
   };
 
   return (
-    <div className="card-group d-flex flex-row">
-      {book.map((item, index) => {
-        const thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-        const amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-        const isExpanded = index === expandedIndex;
+    <div className="card-group-container2">
+      <div className="card-container2">
+        <div className="card-group2">
+          {book.map((item, index) => {
+            const thumbnail =
+              item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+            const amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+            const isExpanded = index === expandedIndex;
 
-        if (thumbnail !== undefined && amount !== undefined) {
-          return (
-            <div className="card" key={index} onClick={() => setShow(true)}>
-              <img src={thumbnail} className="img-fluid rounded-start" alt="Book cover" />
-              <div className="card-body">
-                <h5 className="card-title">{item.volumeInfo.title}</h5>
-                <p className={`card-text ${isExpanded ? 'expanded' : ''}`}>
-                  {isExpanded ? item.volumeInfo.description : `${item.volumeInfo.description.slice(0, 100)}...`}
-                </p>
-                <button className="expand-btn" onClick={() => handleExpandToggle(index)}>
-                  {isExpanded ? 'Read Less' : 'Read More'}
-                </button>
-              </div>
-            </div>
-          );
-        }
-        return null;
-      })}
+            if (thumbnail !== undefined && amount !== undefined) {
+              return (
+                <div className="card2" key={index} onClick={() => handleExpandToggle(index)}>
+                  <img
+                    src={thumbnail}
+                    className="img-fluid rounded-start thumbnailpic"
+                    alt="Book cover"
+                  />
+                  <div className="card-body2">
+                    <h5 className="card-title2">{item.volumeInfo.title}</h5>
+                    <p className={`card-text2 ${isExpanded ? 'expanded' : ''}`}>
+                      {isExpanded
+                        ? item.volumeInfo.description
+                        : `${item.volumeInfo.description.slice(0, 100)}...`}
+                    </p>
+                    <button className="expand-btn" onClick={() => handleExpandToggle(index)}>
+                      {isExpanded ? 'Read Less' : 'Read More'}
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+            return null;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
--- src/js/component/catalog-cards.js
+++ src/js/component/catalog-cards.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Card = () => {
   return (
     <>
-      <div className="card mb-3" style={{ maxWidth: '540px' }}>
+    <div className="card-wrapper">
+      <div className="card mb-3" >
         <div className="row g-0">
           <div className="col-md-4">
             <img src="https://api.time.com/wp-content/uploads/2023/05/best-books-of-the-year-2023-alivingremedy.jpg?quality=85&w=800" className="img-fluid rounded-start" alt="Card" />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-8 card-holder">
             <div className="card-body">
               <h5 className="card-title">1. A Living Remedy, Nicole Chung</h5>
               <p className="card-text">In her first memoir, TIME contributor Nicole Chung described her experience growing up as a Korean American adoptee in a predominantly white town. Her follow-up, A Living Remedy, continues her exploration into identity, this time focusing on her grief after losing both of her parents. Chung’s father died of diabetes and kidney disease in 2018. Then, less than a year later, her mother is diagnosed with cancer and later dies during the COVID-19 pandemic. As Chung wrestles with these overwhelming losses in A Living Remedy, she dissects the inequities inherent to American society by recounting the challenges her parents faced in accessing medical care. The result is a moving portrait of a daughter reckoning with her place in a broken world—and making sense of life without her parents in it.</p>
@@ -17,27 +18,26 @@ const Card = () => {
         </div>
       </div>
 
-      <div className="card mb-3" style={{ maxWidth: '540px' }}>
+      <div className="card mb-3" >
         <div className="row g-0">
           <div className="col-md-4">
             <img src="https://api.time.com/wp-content/uploads/2023/05/best-books-of-the-year-2023-kingalife.jpg?quality=85&w=800" className="img-fluid rounded-start" alt="Card" />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-8 card-holder">
             <div className="card-body">
               <h5 className="card-title">2. </h5>
               <p className="card-text">Jonathan Eig’s book on Martin Luther King Jr. is the first biography of the civil rights icon in decades. It’s a refreshing portrait of King, offering an intimate look inside the life of a man whose massive contributions to American history are known but whose emotional complexities are less so. Eig digs into everything—King’s family origins, his relationship with his wife, the pressures he faced from being so influential so early in his career—to create a portrait of the late activist that captures the dynamic and flawed human that he was. It’s a deftly researched and highly accessible account of a leader, and a new view into the many overlooked parts of King’s story.</p>
-              <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="card mb-3" style={{ maxWidth: '540px' }}>
+      <div className="card mb-3">
         <div className="row g-0">
           <div className="col-md-4">
             <img src="https://api.time.com/wp-content/uploads/2023/05/best-books-of-the-year-2023-ourshareofnight_.jpg?quality=85&w=800" className="img-fluid rounded-start" alt="Card" />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-8 card-holder">
             <div className="card-body">
               <h5 className="card-title">3. Our Share of Night, Mariana Enriquez</h5>
               <p className="card-text">Spanning multiple decades, Argentine author Mariana Enriquez’s weird and wonderful novel, newly translated into English by Megan McDowell, doesn’t fit into just one genre. Oscillating seamlessly between historical fiction and supernatural horror, Our Share of Night centers on Juan and Gaspar, a father and son who are grieving Rosario, the wife and mother they just lost in a car accident. Complicating things is the fact that they are also on the run from the ruthless cult from which Rosario descends. Better known as the Order, the cult will do just about anything to achieve immortality, and Gaspar has developed powers that would make him a valuable asset. Set against a comprehensive backdrop of Argentine history, Our Share of Night offers an absorbing window into a terrifying, fantastical world.</p>
@@ -45,6 +45,7 @@ const Card = () => {
             </div>
           </div>
         </div>
+      </div>
       </div>
     </>
   );
